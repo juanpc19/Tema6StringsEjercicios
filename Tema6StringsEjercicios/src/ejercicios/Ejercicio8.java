cdf531456a332feb5a86c78e964fd2a3e0054535
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-02-20 19:58:11 +0100

    ej 8

--- Tema6StringsEjercicios/src/ejercicios/Ejercicio8.java
+++ Tema6StringsEjercicios/src/ejercicios/Ejercicio8.java
@@ -6,19 +6,44 @@ public class Ejercicio8 {
 
 	public static void main(String[] args) {
 
-		String frase = "";
+		String frase = "";// guardara mensaje con muletillas, lo inicializo vacio
 
-		boolean idiomaJavalandia = false;// guardara si la frase es palindroma o no con true o false
+		String mensaje = "";// guardara mensaje sin muletillas, lo inicializo vacio
 
 		Scanner dogma = new Scanner(System.in);
 
 		System.out.println("Introduzca una frase: ");
 		frase = dogma.nextLine();
 
-		if (frase.startsWith("Javalin, javalon		")==true && (frase.endsWith("		javalén, len, len"))) {
-			idiomaJavalandia=true;
+		// siguiente linea es mensaje de prueba (espacios=2 tabs)
+		// Javalin, javalonsosjavalen, len, len
+		// Javalin, javalon sos javalen, len, len
+
+		// si la cadena empieza por "Javalin, javalon" y acaba en "javalen, len, len"
+		if ((frase.startsWith("Javalin, javalon		") == true && frase.endsWith("		javalen, len, len") == true)) {
+			// elimino las muletillas y me quedo con el mensaje
+			mensaje = frase.substring("Javalin, javalon		".length(),
+					frase.length() - ("		javalen, len, len".length()));
+			// hago print de mensaje
+			System.out.println(mensaje);
+
+			// si la cadena empieza por "Javalin, javalon"
+		} else if (frase.startsWith("Javalin, javalon		") == true) {
+			// elimino la muletilla y me quedo con el mensaje
+			mensaje = frase.substring("Javalin, javalon		".length());
+			// hago print de mensaje
+			System.out.println(mensaje);
+
+			// si la cadena termina en "javalen, len, len"
+		} else if (frase.endsWith("		javalen, len, len") == true) {
+			// elimino la muletilla y me quedo con el mensaje
+			mensaje = frase.substring(0, frase.length() - ("		javalen, len, len".length()));
+			// hago print de mensaje
+			System.out.println(mensaje);
+
 		}
-		//si javalandia true hacer PRINT DEMENSAJE SSIN COLETILLA
+
+		dogma.close();
 	}
 
 }
